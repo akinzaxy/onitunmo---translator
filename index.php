3a548ca9318ef877eda607097fed90e18f968d4c
akinzaXY <lazers@technologist.com>
2020-01-11 20:31:04 +0100

    First Initial commit finitoSqi now 4resme

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Translator</title>
+		<meta name="google-translate-customization">
+		<script type="text/javascript" src="http://cdn.howcode.org/content/static/javascript/jquery.min.js"></script>
+		<script src="http://cdn.howcode.org/content/static/javascript/jquery.cookie.js"></script>	
+		<style type="text/css">
+		.goog-te-banner-frame.skiptranslate{display:none!important;}
+		body{top:0px!important;}
+		</style>
+	</head>
+	<body>
+		<script type="text/javascript">
+		$.cookie('googtrans', '/en/fr');
+		</script>
+		<div id="google_translate_element"></div>
+		<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+		</script>
+		<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+	This is a test website.
+	</body>
+</html>
